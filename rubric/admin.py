57f56932e2e60a0cc8ecac81fe138019da6a6b9a
Andrew Song <andrewsong.j@gmail.com>
2021-08-16 10:25:36 -0400

    rubric admin remove

--- rubric/admin.py
+++ rubric/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from .models import Rubric
-
-class RubricAdmin(admin.ModelAdmin):
-    list_display = ('template', 'points', 'criteria')
-
-# Register your models here.
-
-admin.site.register(Rubric, RubricAdmin)
